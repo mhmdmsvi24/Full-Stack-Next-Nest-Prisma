de7a766e1b90649e69fb4caf127c4d6a03d87829
mhmdmsvi24 <mousavimohammadg@gmail.com>
2024-10-14 20:35:00 +0330

    feat: server actions for user auth added

--- Frontend/app/auth/signup/page.tsx
+++ Frontend/app/auth/signup/page.tsx
@@ -1,18 +1,40 @@
+"use client";
+
 import { Button, Link, Stack, TextField } from "@mui/material";
 
 import NextLink from "next/link";
+import createUser from "./create-user";
+import { useFormState } from "react-dom";
 
 export default function Login() {
+  const [state, formAction] = useFormState(createUser, { error: "" });
+
   return (
-    <Stack spacing={2} className="w-full max-w-xs">
-      <TextField label="Email" variant="outlined" type="email" />
-      <TextField label="Password" variant="outlined" type="password" />
-      <Button variant="contained" color="primary">
-        Signup
-      </Button>
-      <Link component={NextLink} href="/auth/login" className="self-center text-black">
-        Login
-      </Link>
-    </Stack>
+    <form action={formAction} className="w-full max-w-xs">
+      <Stack spacing={2}>
+        <TextField
+          name="email"
+          label="Email"
+          variant="outlined"
+          type="email"
+          helperText={state.error}
+          error={!!state.error}
+        />
+        <TextField
+          name="password"
+          label="Password"
+          variant="outlined"
+          type="password"
+          helperText={state.error}
+          error={!!state.error}
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Signup
+        </Button>
+        <Link component={NextLink} href="/auth/login" className="self-center text-black">
+          Login
+        </Link>
+      </Stack>
+    </form>
   );
 }
